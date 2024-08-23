ab1ae6b81d3fe9cbabf55c7b932ac70581967da1
Deathraymind <116578035+Deathraymind@users.noreply.github.com>
2024-08-23 13:26:37 +0000

    java script update

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const left = document.getElementById("left-side");
 const handleOnMove = e => {
     const p = e.clientX / window.innerWidth * 100;
 
-    left.style.width = '${p}%';
+    left.style.width = `${p}%`;  // Corrected string interpolation with backticks
 }
 
 document.onmousemove = e => handleOnMove(e);
 
-document.ontouchmove = e =>handleOnMove(e.touches[0]);
+document.ontouchmove = e => handleOnMove(e.touches[0]);
