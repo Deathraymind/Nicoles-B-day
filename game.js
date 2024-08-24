36885b9a6e002ffbc96aee69b5462ef3c17c5e22
Deathraymind <116578035+Deathraymind@users.noreply.github.com>
2024-08-24 00:44:24 +0000

    more tweaks

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ let posY = window.innerHeight / 2;
 let velocityX = 0; // Initial horizontal velocity
 let velocityY = 0; // Initial vertical velocity
 let gravity = 0.5; // Gravity acceleration for the ball of yarn
-let friction = 0.96; // Friction to reduce velocity over time
+let friction = 0.9; // Increased friction to reduce velocity over time
 let bounceFactor = 0.4; // Bounce damping factor
 
 let chaserPosX = window.innerWidth / 4;
@@ -118,7 +118,7 @@ function applyPhysics() {
             if (posX < rightLedge.offsetLeft) {
                 posX = rightLedge.offsetLeft - draggable.offsetWidth;
             } else if (posX + draggable.offsetWidth > rightLedge.offsetLeft + rightLedge.offsetWidth) {
-                posX = rightLedge.offsetLeft + rightLedge.offsetWidth;
+                posX = rightLedge.offsetLeft + rightRidge.offsetWidth;
             }
             if (posY < rightLedge.offsetTop) {
                 posY = rightLedge.offsetTop - draggable.offsetHeight;
@@ -130,6 +130,10 @@ function applyPhysics() {
         // Apply friction
         velocityX *= friction;
         velocityY *= friction;
+
+        // Additional velocity reduction
+        velocityX *= 0.9; // Further reduce horizontal velocity
+        velocityY *= 0.9; // Further reduce vertical velocity
 
         // Update the position of the ball of yarn
         draggable.style.left = `${posX}px`;
